a7735e0a44588402c1f283f74df00bd0ef4640da
abbasi <muhammad.ahsan_abbasi@mail.schwarz>
2024-01-30 18:07:37 +0100

    no-ticket: Conditionally enable service plugins. Removed unnecessary plugin

--- scripts/configure_telegraf.sh
+++ scripts/configure_telegraf.sh
@@ -48,12 +48,31 @@ PROM_REMOTE_WRITE_USER=$(echo $TELEGRAF_CONFIG | jq -r '.prom_remote_write_user'
 PROM_REMOTE_WRITE_PASSWD=$(echo $TELEGRAF_CONFIG | jq -r '.prom_remote_write_passwd')
 METRICS_BASIC_AUTH_USERNAME=$(echo $TELEGRAF_CONFIG | jq -r '.metrics_basic_auth_username')
 METRICS_BASIC_AUTH_PASSWORD=$(echo $TELEGRAF_CONFIG | jq -r '.metrics_basic_auth_password')
+SERVICE_METRICS_ENABLED=$(echo $TELEGRAF_CONFIG | jq -r '.service_metrics_enabled')
 
 sed -i 's|prom_remote_write_url|'$PROM_REMOTE_WRITE_URL'|' $TELEGRAF_CONF_FILE
 sed -i 's|prom_remote_write_user|'$PROM_REMOTE_WRITE_USER'|' $TELEGRAF_CONF_FILE
 sed -i 's|prom_remote_write_passwd|'$PROM_REMOTE_WRITE_PASSWD'|' $TELEGRAF_CONF_FILE
 sed -i 's|metrics_basic_auth_username|'$METRICS_BASIC_AUTH_USERNAME'|' $TELEGRAF_CONF_FILE
 sed -i 's|metrics_basic_auth_password|'$METRICS_BASIC_AUTH_PASSWORD'|' $TELEGRAF_CONF_FILE
+
+if [$SERVICE_METRICS_ENABLED == "true"]; then
+  ##Enable RabbitMQ Input Plugin
+  sed -i 's|##\[\[inputs.rabbitmq\]\]|\[\[inputs.rabbitmq\]\]|' $TELEGRAF_CONF_FILE
+  sed -i 's|##url|url|' $TELEGRAF_CONF_FILE
+  sed -i 's|##username|username|' $TELEGRAF_CONF_FILE
+  sed -i 's|##password|password|' $TELEGRAF_CONF_FILE
+
+  ## Enable Mysql Input Plugin
+  sed -i 's|##\[\[inputs.mysql\]\]|\[\[inputs.mysql\]\]|' $TELEGRAF_CONF_FILE
+  sed -i 's|##servers = \[\"mysql_user:mysql_password@tcp(mysql_host:3306)/mysql_name\"\]|servers = \[\"mysql_user:mysql_password@tcp(mysql_host:3306)/mysql_name\"\]|' $TELEGRAF_CONF_FILE
+  sed -i 's|##metric_version|metric_version|' $TELEGRAF_CONF_FILE
+
+  ##Enable Redis Input Plugin
+  sed -i 's|##\[\[inputs.redis\]\]|\[\[inputs.redis\]\]|' $TELEGRAF_CONF_FILE
+  sed -i 's|##servers = \[\"tcp://redis_password@redis_host:6379\"\]|servers = \[\"tcp://redis_password@redis_host:6379\"\]|' $TELEGRAF_CONF_FILE
+  sed -i 's|##insecure_skip_verify = true|insecure_skip_verify = true|' $TELEGRAF_CONF_FILE
+  fi
 
 RABBITMQ_USER=$(echo $VCAP_SERVICES | jq -r '.["appcloud-rabbitmq310"][0].credentials.username')
 RABBITMQ_PASSWORD=$(echo $VCAP_SERVICES | jq -r '.["appcloud-rabbitmq310"][0].credentials.password')
