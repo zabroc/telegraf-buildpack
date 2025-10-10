52a70f2cd3c8e596e5015f9750a3ab700e8d61e3
Martin Holbe <Martin.Holbe@mail.schwarz>
2025-10-10 17:10:17 +0200

    fixed conf

--- scripts/configure_telegraf.sh
+++ scripts/configure_telegraf.sh
@@ -92,17 +92,9 @@ echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 echo ""
 echo ""
 
-# copy REDIS_CACRT to /etc/telegraf
-echo ":::::::::::: Write Redis cert ::::::::::::::::::::::::::::::::::"
-echo ""
-echo ""
-echo $(echo $VCAP_SERVICES | jq -r '.["appcloud-redis7"][0].credentials.cacrt') > /tmp/ca.pem
-
 sed -i 's|redis_host|'$REDIS_HOST'|' $TELEGRAF_CONF_FILE
 sed -i 's|redis_user|'$REDIS_HOST'|' $TELEGRAF_CONF_FILE
 sed -i 's|redis_password|'$REDIS_PASSWORD'|' $TELEGRAF_CONF_FILE
-
-
 
 
 echo ":::::::::::: Telegraf Config ::::::::::::::::::::::::::::::::::"
