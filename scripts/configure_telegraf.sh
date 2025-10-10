37158bdf152cb2c2bdb1c694479eba94255146bf
Martin Holbe <Martin.Holbe@mail.schwarz>
2025-10-10 13:31:48 +0200

    leaflets-monitor: a version for new leaflets-monitor

--- scripts/configure_telegraf.sh
+++ scripts/configure_telegraf.sh
@@ -9,89 +9,45 @@ echo "-----> Configuring $SIDECAR_NAME Sidecar"
 
 TELEGRAF_CONF_FILE=$DEPS_DIR/$DEPS_IDX/telegraf/telegraf.conf
 
-getOrganizationName()
-{
-    echo $(echo $VCAP_APPLICATION | jq -r '.organization_name')
-}
 
-getSpaceName()
-{
-    echo $(echo $VCAP_APPLICATION | jq -r '.space_name')
-}
+## Get Telegraf Config from Environment Variable
+PROM_REMOTE_WRITE_URL=$(echo $TELEGRAF_CONFIG | jq -r '.prom_remote_write_url')
+PROM_REMOTE_WRITE_USER=$(echo $TELEGRAF_CONFIG | jq -r '.prom_remote_write_user')
+PROM_REMOTE_WRITE_PASSWD=$(echo $TELEGRAF_CONFIG | jq -r '.prom_remote_write_passwd')
 
-getApplicationName()
-{
-    echo $(echo $VCAP_APPLICATION | jq -r '.application_name')
-}
+## Write to observability service
+sed -i 's|prom_remote_write_url|'$PROM_REMOTE_WRITE_URL'|' $TELEGRAF_CONF_FILE
+sed -i 's|prom_remote_write_user|'$PROM_REMOTE_WRITE_USER'|' $TELEGRAF_CONF_FILE
+sed -i 's|prom_remote_write_passwd|'$PROM_REMOTE_WRITE_PASSWD'|' $TELEGRAF_CONF_FILE
 
-if [ -z ${ORGANIZATION_NAME+x} ];
-then
-  export ORGANIZATION_NAME=$(getOrganizationName)
-fi
-
-if [ -z ${SPACE_NAME+x} ];
-then
-  export SPACE_NAME=$(getSpaceName)
-fi
-
-if [ -z ${APPLICATION_NAME+x} ];
-then
-  export APPLICATION_NAME=$(getApplicationName)
-fi
+## Extract configs from vcap and substitute placeholders
+ORGANIZATION_NAME=$(echo $VCAP_APPLICATION | jq -r '.organization_name')
+SPACE_NAME=$(echo $VCAP_APPLICATION | jq -r '.space_name')
+APPLICATION_NAME=$(echo $VCAP_APPLICATION | jq -r '.application_name')
 
 sed -i 's|stackit_organization_name|'$ORGANIZATION_NAME'|' $TELEGRAF_CONF_FILE
 sed -i 's|stackit_space_name|'$SPACE_NAME'|' $TELEGRAF_CONF_FILE
 sed -i 's|stackit_application_name|'$APPLICATION_NAME'|' $TELEGRAF_CONF_FILE
 
-PROM_REMOTE_WRITE_URL=$(echo $TELEGRAF_CONFIG | jq -r '.prom_remote_write_url')
-PROM_REMOTE_WRITE_USER=$(echo $TELEGRAF_CONFIG | jq -r '.prom_remote_write_user')
-PROM_REMOTE_WRITE_PASSWD=$(echo $TELEGRAF_CONFIG | jq -r '.prom_remote_write_passwd')
-METRICS_BASIC_AUTH_USERNAME=$(echo $TELEGRAF_CONFIG | jq -r '.metrics_basic_auth_username')
-METRICS_BASIC_AUTH_PASSWORD=$(echo $TELEGRAF_CONFIG | jq -r '.metrics_basic_auth_password')
-SERVICE_METRICS_ENABLED=$(echo $TELEGRAF_CONFIG | jq -r '.service_metrics_enabled')
+RABBITMQ_USER=$(echo $VCAP_SERVICES | jq -r '.["appcloud-rabbitmq40"][0].credentials.username')
+RABBITMQ_PASSWORD=$(echo $VCAP_SERVICES | jq -r '.["appcloud-rabbitmq40"][0].credentials.password')
+RABBITMQ_HOST=$(echo $VCAP_SERVICES | jq -r '.["appcloud-rabbitmq40"][0].credentials.host')
 
-sed -i 's|prom_remote_write_url|'$PROM_REMOTE_WRITE_URL'|' $TELEGRAF_CONF_FILE
-sed -i 's|prom_remote_write_user|'$PROM_REMOTE_WRITE_USER'|' $TELEGRAF_CONF_FILE
-sed -i 's|prom_remote_write_passwd|'$PROM_REMOTE_WRITE_PASSWD'|' $TELEGRAF_CONF_FILE
-sed -i 's|metrics_basic_auth_username|'$METRICS_BASIC_AUTH_USERNAME'|' $TELEGRAF_CONF_FILE
-sed -i 's|metrics_basic_auth_password|'$METRICS_BASIC_AUTH_PASSWORD'|' $TELEGRAF_CONF_FILE
-
-if [ $SERVICE_METRICS_ENABLED == "true" ]; then
-  ##Enable Redis Input Plugin
-  sed -i 's|##\[\[inputs.redis\]\]|\[\[inputs.redis\]\]|' $TELEGRAF_CONF_FILE
-  sed -i 's|##servers = \[\"tcp://:redis_password@redis_host:6379\"\]|servers = \[\"tcp://:redis_password@redis_host:6379\"\]|' $TELEGRAF_CONF_FILE
-  sed -i 's|##insecure_skip_verify = true|insecure_skip_verify = true|' $TELEGRAF_CONF_FILE
-
-  ##Enable RabbitMQ Input Plugin
-  sed -i 's|##\[\[inputs.rabbitmq\]\]|\[\[inputs.rabbitmq\]\]|' $TELEGRAF_CONF_FILE
-  sed -i 's|##url|url|' $TELEGRAF_CONF_FILE
-  sed -i 's|##username|username|' $TELEGRAF_CONF_FILE
-  sed -i 's|##password|password|' $TELEGRAF_CONF_FILE
-  sed -i 's|##metric_exclude = \[\]|metric_exclude = \[\"federation\", \"node\"\]|' $TELEGRAF_CONF_FILE
-
-  ## Enable Mysql Input Plugin
-  sed -i 's|##\[\[inputs.mysql\]\]|\[\[inputs.mysql\]\]|' $TELEGRAF_CONF_FILE
-  sed -i 's|##servers = \[\"mysql_user:mysql_password@tcp(mysql_host:3306)/mysql_name\"\]|servers = \[\"mysql_user:mysql_password@tcp(mysql_host:3306)/mysql_name\"\]|' $TELEGRAF_CONF_FILE
-  sed -i 's|##metric_version|metric_version|' $TELEGRAF_CONF_FILE
-  fi
-
-RABBITMQ_USER=$(echo $VCAP_SERVICES | jq -r '.["appcloud-rabbitmq310"][0].credentials.username')
-RABBITMQ_PASSWORD=$(echo $VCAP_SERVICES | jq -r '.["appcloud-rabbitmq310"][0].credentials.password')
-RABBITMQ_HOST=$(echo $VCAP_SERVICES | jq -r '.["appcloud-rabbitmq310"][0].credentials.host')
 sed -i 's|rabbitmq_user|'$RABBITMQ_USER'|' $TELEGRAF_CONF_FILE
 sed -i 's|rabbitmq_password|'$RABBITMQ_PASSWORD'|' $TELEGRAF_CONF_FILE
 sed -i 's|rabbitmq_host|'$RABBITMQ_HOST'|' $TELEGRAF_CONF_FILE
 
-MYSQL_USER=$(echo $VCAP_SERVICES | jq -r '.["appcloud-mariadb106"][0].credentials.username')
-MYSQL_PASSWORD=$(echo $VCAP_SERVICES | jq -r '.["appcloud-mariadb106"][0].credentials.password')
-MYSQL_HOST=$(echo $VCAP_SERVICES | jq -r '.["appcloud-mariadb106"][0].credentials.host')
-MYSQL_NAME=$(echo $VCAP_SERVICES | jq -r '.["appcloud-mariadb106"][0].credentials.name')
-sed -i 's|mysql_user|'$MYSQL_USER'|' $TELEGRAF_CONF_FILE
-sed -i 's|mysql_password|'$MYSQL_PASSWORD'|' $TELEGRAF_CONF_FILE
-sed -i 's|mysql_host|'$MYSQL_HOST'|' $TELEGRAF_CONF_FILE
-sed -i 's|mysql_name|'$MYSQL_NAME'|' $TELEGRAF_CONF_FILE
+MARIADB_USER=$(echo $VCAP_SERVICES | jq -r '.["appcloud-mariadb106"][0].credentials.username')
+MARIADB_PASSWORD=$(echo $VCAP_SERVICES | jq -r '.["appcloud-mariadb106"][0].credentials.password')
+MARIADB_HOST=$(echo $VCAP_SERVICES | jq -r '.["appcloud-mariadb106"][0].credentials.host')
+MARIADB_NAME=$(echo $VCAP_SERVICES | jq -r '.["appcloud-mariadb106"][0].credentials.name')
 
-REDIS_PASSWORD=$(echo $VCAP_SERVICES | jq -r '.["appcloud-redis60"][0].credentials.password')
+sed -i 's|mariadb_user|'$MARIADB_USER'|' $TELEGRAF_CONF_FILE
+sed -i 's|mariadb_password|'$MARIADB_PASSWORD'|' $TELEGRAF_CONF_FILE
+sed -i 's|mariadb_host|'$MARIADB_HOST'|' $TELEGRAF_CONF_FILE
+sed -i 's|mariadb_name|'$MARIADB_NAME'|' $TELEGRAF_CONF_FILE
+
+REDIS_PASSWORD=$(echo $VCAP_SERVICES | jq -r '.["appcloud-redis7"][0].credentials.password')
 REDIS_HOST=$(echo $VCAP_SERVICES | jq -r '.["appcloud-redis60"][0].credentials.host')
 sed -i 's|redis_password|'$REDIS_PASSWORD'|' $TELEGRAF_CONF_FILE
 sed -i 's|redis_host|'$REDIS_HOST'|' $TELEGRAF_CONF_FILE
